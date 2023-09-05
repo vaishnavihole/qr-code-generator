1e2cf54b2ecb9ae7874a93416933b7b2d261ee63
vaishnavihole <vaishnavihole1@gmail.com>
2023-09-05 12:25:16 +0530

    feat: QR code generation on text change

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [qrText, setQrText] = useState('');
   return (
     <>
       <div className='container'>
         <p >Enter your text or URL</p>
 
-        <input type='text' placeholder='Text or URL' />
+        <input type='text' placeholder='Text or URL' value={qrText}
+         onChange={(e)=>{
+          setQrText(e.target.value);
+        }}/>
 
         <div className='imgBox'>
-          <img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example" className='qr-code-img' />
-
-          <button className='btn-generator'>GeneratorQR Code</button>
-
+          <img src={`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText}`} 
+          className='qr-code-img' />
         </div>
+
+        <button type='button' className='btn-download'>Download QR</button>
 
       </div>
     </>
